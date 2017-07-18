b10042d441721131a45bd88714df12c754f81289
Borys Cherkas <cherkas.borys.igorevich@gmail.com>
2017-07-18 20:24:46 +0300

    Removed unused code

--- src/VehicleShop/Startup.cs
+++ src/VehicleShop/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using VehicleShop.Data;
 using VehicleShop.Models;
-using VehicleShop.Services;
 
 namespace VehicleShop
 {
@@ -53,10 +52,6 @@ namespace VehicleShop
                 .AddDefaultTokenProviders();
 
             services.AddMvc();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
