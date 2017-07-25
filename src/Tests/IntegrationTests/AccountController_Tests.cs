d8306da8c51d435d2c8f0ce1c7881631ee715bd2
Borys Cherkas <cherkas.borys.igorevich@gmail.com>
2017-07-25 23:38:24 +0300

    Added empty integration tests project;

---
+++ src/Tests/IntegrationTests/AccountController_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class AccountController_Tests : BaseIntegrationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
